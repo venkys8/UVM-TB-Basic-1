238c88dbeaa3c640dcd1a26cea114243dbf3f9b9
venkys8 <138795338+venkys8@users.noreply.github.com>
2024-05-09 16:26:01 +0530

    Update generator.sv

--- combinational_adder/generator.sv
+++ combinational_adder/generator.sv
@@ -1,20 +1,20 @@
-class generator extends uvm_sequence#(transaction);
-  `uvm_object_utils(generator)
-  
+class generator extends uvm_sequence #(transaction);
+`uvm_object_utils(generator)
+ 
   transaction t;
-  integer i;
-  
-  function new(string name="gen");
+ 
+  function new(string name = "GEN");
     super.new(name);
   endfunction
-  
-  virtual task body();
-    t=transaction::type_id::create("t");
-    repeat(10) begin
-      start_item(t);
-      t.randomize();
-      `uvm_info("GEN",$psprintf("data sent to driver, a=%0d,b=%0d",t.a,t.b),UVM_LOW)
-      finish_item(t);
-    end
-  endtask
+ 
+virtual task body();
+  t = transaction::type_id::create("t");
+  repeat(10) begin
+    start_item(t);
+    t.randomize();
+    `uvm_info("GENERATOR",$sformatf("Data send to Driver a :%0d , b :%0d",t.a,t.b), UVM_NONE);
+    finish_item(t);
+  end
+endtask
+ 
 endclass
