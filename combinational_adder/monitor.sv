ffafb2baf924417274399c4dfc76d27733c35f48
venkys8 <138795338+venkys8@users.noreply.github.com>
2024-05-09 16:26:41 +0530

    Update monitor.sv

--- combinational_adder/monitor.sv
+++ combinational_adder/monitor.sv
@@ -1,34 +1,32 @@
 class monitor extends uvm_monitor;
-  `uvm_component_utils(monitor)
-  
-  uvm_analysis_port#(transaction) send;
-  
-  function new(string name="MON",uvm_component parent=null);
-    super.new(name,parent);
-    send=new("send",this);
+`uvm_component_utils(monitor)
+ 
+uvm_analysis_port #(transaction) send;
+ 
+  function new(string name = "MONITOR", uvm_component parent = null);
+    super.new(name, parent);
+    send = new("send", this);
   endfunction
-  
+ 
   transaction t;
   virtual add_if vif;
-  
+ 
   virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    t=transaction::type_id::create("t");
+   super.build_phase(phase);
+    t = transaction::type_id::create("t");
     
-    if(!uvm_config_db#(virtual add_if)::get(this,"","vif",vif))
-      `uvm_error("MONITOR","Unable to get interface in monitor");
+    if(!uvm_config_db #(virtual add_if)::get(this,"","vif",vif)) 
+   `uvm_error("MON","Unable to access uvm_config_db");
   endfunction
-  
-  virtual task run_phase(uvm_phase phase);
+ 
+    virtual task run_phase(uvm_phase phase);
     forever begin
-      #10;
-      t.a=vif.a;
-      t.b=vif.b;
-      t.y=vif.y;
-      `uvm_info("MONITOR",$psprintf("Data sent to scoreboard. a=%0d, b=%0d, y=%0d",t.a,t.b,t.y),UVM_LOW)
-      send.write(t);
+    #10;
+    t.a = vif.a;
+    t.b = vif.b;
+    t.y = vif.y;
+      `uvm_info("MONITOR", $sformatf("Data send to Scoreboard a : %0d , b : %0d and y : %0d", t.a,t.b,t.y), UVM_NONE);
+    send.write(t);
     end
-  endtask    
+    endtask
 endclass
-  
-  
