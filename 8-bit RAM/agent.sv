091e8f7bfd9f8a8d0cedbc989fcbea7e8b2fc79a
venkys8 <138795338+venkys8@users.noreply.github.com>
2024-05-14 17:16:41 +0530

    Create agent.sv

---
+++ 8-bit RAM/agent.sv
@@ -0,0 +1,23 @@
+class agent extends uvm_agent;
+  `uvm_component_utils(agent)
+  
+  function new(string name="AGENT", uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+  
+  monitor mon;
+  driver drv;
+  uvm_sequencer#(transaction) seqr;
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon=monitor::type_id::create("mon",this);
+    drv=driver::type_id::create("drv",this);
+    seqr=uvm_sequencer#(transaction)::type_id::create("seqr",this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    drv.seq_item_port.connect(seqr.seq_item_export);
+  endfunction
+endclass
