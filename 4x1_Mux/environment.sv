c06c4ce5097db2fe06d0c003041f886df9f0a427
venkys8 <138795338+venkys8@users.noreply.github.com>
2024-06-20 11:10:13 +0530

    Create environment.sv

---
+++ 4x1_Mux/environment.sv
@@ -0,0 +1,24 @@
+class env extends uvm_env;
+`uvm_component_utils(env)
+ 
+ 
+  function new(string name = "ENV", uvm_component parent=null);
+    super.new(name, parent);
+  endfunction
+ 
+scoreboard sbd;
+agent agt;
+ 
+virtual function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+  sbd = scoreboard::type_id::create("sbd",this);
+  agt = agent::type_id::create("agt",this);
+endfunction
+ 
+ 
+virtual function void connect_phase(uvm_phase phase);
+super.connect_phase(phase);
+  agt.mon.send.connect(sbd.recv);
+endfunction
+ 
+endclass
